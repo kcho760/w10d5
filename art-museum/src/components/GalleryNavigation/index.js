411a5a6f8db685fdbfa78bb45e28e1ab78aa152e
kcho760 <kcho760@gmail.com>
2023-05-12 17:23:56 -0400

    gallery view started

--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,16 +1,9 @@
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { NavLink } from "react-router-dom";
 import "./GalleryNavigation.css";
 
 function GalleryNavigation({galleries}) {
     // console.log({galleries}); //This just wraps in an obj?
     // console.log(galleries);
-    
-
-
-
-
-
-
     
     const galleryLinks = galleries.map(gallery => {
         return <li key={`${gallery.id}`}><NavLink to={`/galleries/${gallery.id}`}>{`${gallery.name}`}</NavLink></li>;
---
+++ art-museum/src/components/GalleryView/index.js
@@ -0,0 +1,7 @@
+import { useParams } from "react-router-dom";
+function GalleryView() {
+    return (
+    <h1>Yello from Gallery View</h1>
+    )
+}
+export default GalleryView;
